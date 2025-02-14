33c9130fe49d0e867a175fa49ed1efa73f1729e1
Yen-Ting Li <ytli.tw@gmail.com>
2025-02-15 03:07:08 +0800

    handle no language in deploy

--- api/routes/k8s_deploy.py
+++ api/routes/k8s_deploy.py
@@ -5,13 +5,14 @@ import json
 from utils.k8s.job import create_configmap_from_file, deploy_job, delete_configmap, load_kube_config
 import random
 import os
+from utils.chat import detect_code_language
 
 router = APIRouter()
 
 
 class K8sRequest(BaseModel):
     code: str
-    language: Literal["python3", "java21"]
+    language: Literal["python3", "java21"] = None
 
 class K8sResponse(BaseModel):
     status: str
@@ -21,6 +22,16 @@ class K8sResponse(BaseModel):
 @router.post("/k8s", response_model=K8sResponse)
 def run_code(request: K8sRequest):
     """Runs user-provided code in a Kubernetes job and fetches logs."""
+
+    if not request.language:
+        detected_language = detect_code_language(request.code)
+        if detected_language.startswith("python"):
+            request.language = "python3"
+        elif detected_language.startswith("java"):
+            request.language = "java21"
+        else:
+            raise HTTPException(status_code=400, detail="Language not supported")
+
     load_kube_config()
     configmap_name = f"configmap-{random.randint(1, 1000000000)}"
     
