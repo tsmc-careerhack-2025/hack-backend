bc13ea4c942a932096860234268488d9a8d4a3f9
Yen-Ting Li <ytli.tw@gmail.com>
2025-02-14 19:19:19 +0800

    test: deploy dev

--- k8sdeploy/job-deploy.py
+++ k8sdeploy/job-deploy.py
@@ -62,7 +62,7 @@ def deploy_job(yaml_file, new_configmap_name,  code_filename, language: str = "p
     api_instance = client.BatchV1Api()
     core_api = client.CoreV1Api()
     namespace = job_manifest["metadata"].get("namespace", "default")
-    job_name = job_manifest["metadata"]["name"] + random.randint(1, 100000000) 
+    job_name = job_manifest["metadata"]["name"] + str(random.randint(1, 100000000))
     job_manifest["spec"]["template"]["spec"]["volumes"][0]["configMap"]["name"] = new_configmap_name
 
     # set command based on language
