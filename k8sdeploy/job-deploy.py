7c3df9a1304c59fbc207307f07cb54cc3570293c
Yen-Ting Li <ytli.tw@gmail.com>
2025-02-14 15:09:27 +0800

    test: deploy dev

---
+++ k8sdeploy/job-deploy.py
@@ -0,0 +1,26 @@
+import yaml
+from kubernetes import client, config
+
+def load_kube_config():
+    """Load Kubernetes config."""
+    try:
+        config.load_kube_config()  # Use local kubeconfig
+    except:
+        config.load_incluster_config()  # Use in-cluster config if running inside GKE
+
+def deploy_job(yaml_file):
+    """Deploy a job from a YAML file to the GKE cluster."""
+    with open(yaml_file, "r") as file:
+        job_manifest = yaml.safe_load(file)
+
+    api_instance = client.BatchV1Api()
+    namespace = job_manifest["metadata"].get("namespace", "default")
+
+    response = api_instance.create_namespaced_job(
+        body=job_manifest, namespace=namespace
+    )
+    print(f"Job {job_manifest['metadata']['name']} created in namespace {namespace}")
+
+if __name__ == "__main__":
+    load_kube_config()
+    deploy_job("job.yaml")  # Replace with your YAML file path
