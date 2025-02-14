e4365fd7f7942947d8d3953d14b120b2e4bce058
GUAN MING <guanmingchiu@gmail.com>
2025-02-14 16:41:41 +0800

    env: update init aiplatform

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.main import api_router
 import os
 from dotenv import load_dotenv
+from google.cloud import aiplatform
 
 # FastAPI app
 app = FastAPI()
@@ -12,6 +13,7 @@ load_dotenv()
 os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
 os.environ["LANGSMITH_TRACING"] = os.getenv("LANGSMITH_TRACING")
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "serviceAccountKey.json"
+aiplatform.init(project=os.getenv("PROJECT_ID"), location=os.getenv("LOCATION"))
 
 origins = ["*"]
 app.add_middleware(
