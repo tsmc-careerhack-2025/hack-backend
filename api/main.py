3a5c21312fdf97a8899128faa845b3632fb62de0
Yen-Ting Li <ytli.tw@gmail.com>
2025-02-14 19:59:16 +0800

    add deploy route 2

--- api/main.py
+++ api/main.py
@@ -4,11 +4,13 @@ from api.routes.upgrade import router as upgrade_router
 from api.routes.covert import router as covert_router
 from api.routes.optimize import router as optimize_router
 from api.routes.deploy import router as deploy_router
+from api.routes.k8s_deploy import router as k8s_deploy_router
 
 api_router = APIRouter()
 api_router.include_router(upgrade_router)
 api_router.include_router(covert_router)
 api_router.include_router(optimize_router)
 api_router.include_router(deploy_router)
+api_router.include_router(k8s_deploy_router)
 
 
