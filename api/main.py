0fe206a7eb1f32b229ae5642122681f8729a7777
GUAN MING <guanmingchiu@gmail.com>
2025-02-19 17:26:49 +0800

    fix: update router name

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from api.routes.upgrade import router as upgrade_router
-from api.routes.covert import router as covert_router
+from api.routes.convert import router as convert_router
 from api.routes.optimize import router as optimize_router
 from api.routes.k8s_deploy import router as k8s_deploy_router
 from api.routes.correct import router as correct_router
@@ -9,7 +9,7 @@ from api.routes.detect import router as detect_router
 
 api_router = APIRouter()
 api_router.include_router(upgrade_router)
-api_router.include_router(covert_router)
+api_router.include_router(convert_router)
 api_router.include_router(optimize_router)
 api_router.include_router(k8s_deploy_router)
 api_router.include_router(correct_router)
